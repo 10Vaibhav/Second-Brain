a28ff14fd6d4cfbeb4b0932b7a1768d3d0060c1d
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-03-27 21:04:50 +0530

    create a ShareIcon Component and fixing the size and other little issues

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { Button } from './components/Button'
 import { PlusIcon } from './icons/PlusIcon'
+import { ShareIcon } from './icons/ShareIcon'
 
 function App() {
 
   return <div className='flex justify-center items-center space-x-4'>
-    <Button size={"sm"} variant={"primary"} text={"Share"}  />
+    <Button startIcon={<ShareIcon/>} size={"sm"} variant={"primary"} text={"Share"}  />
 
-    <Button startIcon={<PlusIcon size={"md"}/>} size={"sm"} variant={"secondary"} text={"Add Content"} />
+    <Button startIcon={<PlusIcon/>} size={"sm"} variant={"secondary"} text={"Add Content"} />
   </div>
 }
 
--- Frontend/src/components/Button.tsx
+++ Frontend/src/components/Button.tsx
@@ -1,3 +1,4 @@
+import { ReactElement } from "react";
 
 type variants = "primary" | "secondary";
 
@@ -6,8 +7,8 @@ export interface ButtonProps {
     variant: variants;
     size: "sm" | "md" | "lg";
     text: string;
-    startIcon?: any;
-    endIcon?: any;
+    startIcon?: ReactElement;
+    endIcon?: ReactElement;
     onClick?: ()=> void;
 }
 
@@ -17,12 +18,12 @@ const variantStyles = {
 }
 
 const sizeStyles = {
-    "sm": "p-2",
-    "md": "p-4",
-    "lg": "p-6"
+    "sm": "p-2 text-sm",
+    "md": "p-4 text-md",
+    "lg": "p-6 text-lg"
 }
 
 export const Button = (props: ButtonProps) => {
 
-    return <button className={`${variantStyles[props.variant]} ${sizeStyles[props.size]} m-2 rounded-md w-[160px] cursor-pointer hover:scale-105 flex justify-center items-center`}>{props.startIcon ? <div className="pr-2">{props.startIcon}</div>: null} {props.text}</button>
+    return <button className={`${variantStyles[props.variant]} ${sizeStyles[props.size]} m-2 rounded-md ${props.size === "lg"? "w-auto": "w-[160px]"} cursor-pointer hover:scale-105 flex justify-center items-center`}>{props.startIcon ? <div className="pr-2">{props.startIcon}</div>: null} {props.text}</button>
 }
