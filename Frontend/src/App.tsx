ffe3df18b545e4ceec2a6225b45c4bfa93e605cf
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-03-27 19:31:30 +0530

    Build a Button UI Component like library

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
 import { Button } from './components/Button'
+import { PlusIcon } from './icons/PlusIcon'
 
 function App() {
 
-  return <div>
-    <Button endIcon={"+"} startIcon={"-"} variant="primary" size="md" onClick={()=> {}} text={"abc"}  />
+  return <div className='flex justify-center items-center space-x-4'>
+    <Button size={"sm"} variant={"primary"} text={"Share"}  />
+
+    <Button startIcon={<PlusIcon size={"md"}/>} size={"sm"} variant={"secondary"} text={"Add Content"} />
   </div>
 }
 
--- Frontend/src/components/Button.tsx
+++ Frontend/src/components/Button.tsx
@@ -1,19 +1,31 @@
 
 
+type variants = "primary" | "secondary";
 
 export interface ButtonProps {
 
-    variant: "primary" | "secondary";
+    variant: variants;
     size: "sm" | "md" | "lg";
     text: string;
     startIcon?: any;
     endIcon?: any;
-    onClick: ()=> void;
+    onClick?: ()=> void;
+}
+
+const variantStyles = {
+    "primary" : "bg-[#5046e4] text-white",
+    "secondary": "bg-[#e0e7fe] text-[#5046e4]"
+}
+
+const sizeStyles = {
+    "sm": "p-2",
+    "md": "p-4",
+    "lg": "p-6"
 }
 
 export const Button = (props: ButtonProps) => {
 
-    return <button></button>
+    return <button className={`${variantStyles[props.variant]} ${sizeStyles[props.size]} m-2 rounded-md w-[160px] cursor-pointer hover:scale-105 flex justify-center items-center`}>{props.startIcon ? <div className="pr-2">{props.startIcon}</div>: null} {props.text}</button>
 }
 
-<Button endIcon={"+"} startIcon={"-"} variant="primary" size="md" onClick={()=> {}} text={"abc"}  />
+{/* <Button endIcon={"+"} startIcon={"-"} variant="primary" size="md" onClick={()=> {}} text={"abc"}  /> */}
