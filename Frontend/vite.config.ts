36414b72f182c7609b33ec7ecbe3045e10670e15
vaibhav mahajan <vaibhavmahajan43810@gmail.com>
2025-12-31 21:16:31 +0000

    try to fix

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -2,21 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
+  plugins: [react(),
     tailwindcss(),
   ],
-  build: {
-    // Generate hashed filenames for cache busting
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    },
-    sourcemap: false,
-    emptyOutDir: true,
-  }
 })
